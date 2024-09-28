6e4976de9b864acf520b4b503cf665d3673bcec6
jica <jica.etzc@gmail.com>
2024-09-28 14:57:01 +0530

    Refactor chapter rendering to use paged content and add react-native-render-html dependency

--- app/chapters/content.tsx
+++ app/chapters/content.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, useWindowDimensions, ScrollView } from 'react-native';
 import PagerView from 'react-native-pager-view';
+import RenderHtml from 'react-native-render-html';
 
 export default function RenderPagedContent({ content }: { content: string }) {
     const [pages, setPages] = useState<any[]>([]);
-
+    const { width } = useWindowDimensions();
     // Function to split content into pages
     const splitContentIntoPages = (content: string) => {
         const words = content.split(' ');
-        const pageSize = 205;
+        const pageSize = 20500;
         const result = [];
-        
+
 
         for (let i = 0; i < words.length; i += pageSize) {
             result.push(words.slice(i, i + pageSize).join(' '));
@@ -24,15 +25,25 @@ export default function RenderPagedContent({ content }: { content: string }) {
         setPages(pageContent);
     }, [content]);
 
+
+
     return (
         <PagerView style={{ flex: 1 }} initialPage={0}>
-            {pages.map((pageContent, index) => (
-                <View key={index} style={{ padding: 20 }}>
-                    <Text style={{ fontSize: 18 }}>
-                        {pageContent}
-                    </Text>
-                </View>
-            ))}
+            {pages.map((eventsHTML, index) => {
+                const source = { html: eventsHTML };
+                return (
+                    <View key={index} style={{ flex: 1, marginHorizontal: 10 }}>
+                        <ScrollView>
+                            <RenderHtml
+                                contentWidth={width}
+                                source={source}
+                                baseStyle={{ fontSize: 18 }}
+                            />
+                            <View style={{ margin: 20 }} ></View>
+                        </ScrollView>
+                    </View>
+                );
+            })}
         </PagerView>
     );
 };
