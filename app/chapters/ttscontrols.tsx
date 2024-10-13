7c611e0d3c76a37b0ba56b5c8fcf6634c1440887
jica <jica.etzc@gmail.com>
2024-10-13 22:26:06 +0530

    Refactor TTS module to include voice selection dropdown

--- app/chapters/ttscontrols.tsx
+++ app/chapters/ttscontrols.tsx
@@ -1,11 +1,12 @@
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { TTS, ttsStore, SpeechAction, setTTS, isSpeechOrPause } from "./tts";
-import { Button, IconButton, Title, useTheme } from "react-native-paper";
+import { Button, Icon, IconButton, Title, useTheme } from "react-native-paper";
 import React, { useEffect } from "react";
 import Slider from '@react-native-community/slider';
 import { defaultTTSConfig, TTSConfig, UserPreferences, userPrefStore } from "../userpref";
 import { MD3Colors } from "react-native-paper/lib/typescript/types";
 import * as Speech from 'expo-speech';
+import SelectDropdown from 'react-native-select-dropdown'
 
 type SetTTSConfig = (ttsConfig: TTSConfig) => void;
 
@@ -16,6 +17,7 @@ export default function TTSControls() {
     const setTTStore: (tts: TTS) => void = ttsStore((state: any) => state.setTTS);
     const updateTTSConfig: SetTTSConfig = ttsStore((state: any) => state.updateTTSConfig);
     const controlVisible = isSpeechOrPause(tts.state);
+    const [voices, setVoices] = React.useState<Speech.Voice[] | null>(null);
     const colors = useTheme().colors;
 
     function updateTTS(state: SpeechAction) {
@@ -30,9 +32,53 @@ export default function TTSControls() {
 
     useEffect(() => {
         Speech.getAvailableVoicesAsync().then((voices) => {
-            console.log(voices);
+            if (voices.length) {
+                setVoices([
+                    {
+                        identifier: 'system', name: 'System Default',
+                        quality: Speech.VoiceQuality.Default, language: 'System Default'
+                    },
+                    ...voices,
+                ]);
+            }
         });
-    }, []);
+    }, [ttsConfig]);
+
+    const voicesSelect = (voices?.length && <>
+        <SelectDropdown
+            defaultValue={ttsConfig.voice}
+            data={voices.map((voice) => ({ title: voice.name, id: voice.identifier, label: voice.language }))}
+            onSelect={(item, _) => {
+                updateTTSConfigBoth({ ...ttsConfig, voice: item.id });
+            }}
+            defaultValueByIndex={voices.findIndex((voice) => voice.identifier === ttsConfig.voice)}
+            renderButton={(__, _) => {
+                const voice = voices.find((voice) => voice.identifier === ttsConfig.voice) || voices[0];
+                return (
+                    <Button mode="contained-tonal" style={styles.dropdownButtonStyle}>
+                        <Icon source="microphone" size={20} />
+                        <View style={{ width: 8 }} />
+                        <Title style={{ fontSize: 16 }} >{voice.language}</Title>
+                    </Button>
+                );
+            }}
+            renderItem={(item, _, isSelected) => {
+                return (
+                    <View style={[
+                        styles.dropdownItemStyle,
+                        [{ backgroundColor: colors.background, ...(isSelected && { backgroundColor: colors.primary }) }]
+                    ]}>
+                        <Text style={[styles.dropdownItemTxtStyle, {
+                            color: colors.primary,
+                            ...(isSelected && { color: colors.background })
+                        }]}>{item.label}</Text>
+                    </View>
+                );
+            }}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={[styles.dropdownMenuStyle, { backgroundColor: colors.surface }]}
+        />
+    </>);
 
     const controlButtons = <>
         <View style={styles.bottomBarStyle}>
@@ -40,8 +86,11 @@ export default function TTSControls() {
             <View style={{ width: 16 }} />
             <IconButton icon={tts.state === 'pause' ? 'play' : 'pause'}
                 onPress={() => updateTTS(tts.state === 'pause' ? 'speak' : 'pause')} />
+            <View style={{ width: 16 }} />
+            {voicesSelect}
         </View>
     </>;
+
     const ttsConfigOptions = <>
         <View style={{ paddingHorizontal: 10 }}>
             <SliderControl
@@ -85,11 +134,54 @@ export default function TTSControls() {
     </>);
 }
 
-const styles = {
+const styles = StyleSheet.create({
     bottomBarStyle: {
         flexDirection: 'row' as 'row', justifyContent: 'center' as 'center',
     },
-};
+    dropdownButtonStyle: {
+        width: 200,
+        height: 50,
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 12,
+    },
+    dropdownButtonTxtStyle: {
+        flex: 1,
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#151E26',
+    },
+    dropdownButtonArrowStyle: {
+        fontSize: 28,
+    },
+    dropdownButtonIconStyle: {
+        fontSize: 28,
+        marginRight: 8,
+    },
+    dropdownMenuStyle: {
+        width: 200,
+        borderRadius: 8,
+    },
+    dropdownItemStyle: {
+        width: '100%',
+        flexDirection: 'row',
+        paddingHorizontal: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 8,
+    },
+    dropdownItemTxtStyle: {
+        flex: 1,
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    dropdownItemIconStyle: {
+        fontSize: 28,
+        marginRight: 8,
+    },
+});
 
 function SliderControl({ label, value, onValueChange, min, max, step, colors, defaultValue }:
     {
